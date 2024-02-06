ad99e2a1f30695d05f78af02dd6271d8eaeace6c
dillon <dillon_patrick@outlook.com>
2024-02-05 22:27:22 -0300

    Adding finalizers, setup and teardown to tests

--- tests/test_load.py
+++ tests/test_load.py
@@ -1,10 +1,39 @@
+import os
+import uuid
 import pytest
 from dundie.core import load
 from tests.constants import PEOPLE_FILE
 
 
+@pytest.fixture(scope="function", autouse=True)
+def create_new_file(tmpdir):
+    file_ = tmpdir.join("new_file.txt")
+    file_.write("Isso é sujeira...")
+    yield
+    file_.remove()
+
+
 @pytest.mark.unit
-def test_load():
+@pytest.mark.high
+def test_load(request):
     """Test load function"""
+    filepath = f"arquivo_indesejado-{uuid.uuid4()}.txt"
+    request.addfinalizer(lambda: os.unlink(filepath))
+
+    with open(filepath, "w") as file_:
+        file_.write("dados uteis somente para teste")
+
+    assert len(load(PEOPLE_FILE)) == 2
+    assert load(PEOPLE_FILE)[0][0] == "J"
+
+
+@pytest.mark.unit
+@pytest.mark.high
+def test_load2():
+    """Test load function"""
+
+    with open(f"arquivo_indesejado-{uuid.uuid4()}.txt", "w") as file_:
+        file_.write("dados uteis somente para teste")
+
     assert len(load(PEOPLE_FILE)) == 2
     assert load(PEOPLE_FILE)[0][0] == "J"
